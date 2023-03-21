4c25c2097e46421a5be13ee74af110447f153388
stephan <smuts1989@gmail.com>
2023-03-21 14:27:37 +0100

    Cells now work with guesses

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,12 +1,17 @@
 import React from "react";
+import { range } from '../../../src/utils';
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 
 function GuessResults({ guesses }) {
   return (
     <div className="guess-results">
-      {guesses.map((guess) => (
-        <p key={Math.random()} className="guess">{guess}</p>
-      ))}
-      
+      {range(NUM_OF_GUESSES_ALLOWED).map((i) => (
+        <p key={Math.random()} className="guess">
+          {range(5).map((j) => (
+            <span key={Math.random()} className="cell">{guesses[i]?.charAt(j)}</span>
+          ))}
+        </p>
+      ))}      
     </div>
     );
 }
